ac4522b20cc2718f08c6b6f2d2834b49b596e55f
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-04-18 16:44:53 -0400

    commented everything

--- stack1.asm
+++ stack1.asm
@@ -6,9 +6,9 @@
 	addi $t3,$t1,5		#x+5 in $t3
 	mul $t4,$t2,$t3		#store ()*() into $t4
 
-	li $v0,1
-	move $a0,$t4
+	li $v0,1		#display the results
+	move $a0,$t4		#get the result into a0
 	syscall
 	
-	li $v0,10
+	li $v0,10		#terminate the code
 	syscall
