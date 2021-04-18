0d2a7751cc8052e57303e0230a4036a18ad102b6
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-04-18 17:55:35 -0400

    structured it for a loop

--- stack2.asm
+++ stack2.asm
@@ -1,8 +1,17 @@
 .text 
 
-	la $s0,nm 	#loads nm (the random six values that are stored in nm) into $s0
+
+#index counter -number iteraties 
+#we need to run the code 6 times for the 6 values $s2<-- 6
+#s0- location for the number we need to process
+#$s1 and $s2 is what bounds the iterations
+
+	la $s0,nm 	#loads nm (the random six values that are stored in nm) into $s0-- base address
 	
-	lw $t1, 0($s0)	#gets the first value stored in $s0 and stores it into $t1
+	li $s1,0	#how many numbers did we process, like iteration counter
+	addi $s2,$zero,6	#we need to do this 
+	
+loop:	lw $t1, 0($s0)	#gets the first value stored in $s0 and stores it into $t1
 	
 	addi $t1,$t0,24		#test value for x is 12
 	addi $t2,$t1,-2		#x-2 in $t2
@@ -12,6 +21,8 @@
 	li $v0,1		#display the results
 	move $a0,$t4		#get the result into a0
 	syscall
+	
+	
 	
 	li $v0,10		#terminate the code
 	syscall	
