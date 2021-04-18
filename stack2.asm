a84fc9d9975d686aff5d8a8f844b15a98140d233
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-04-18 16:52:27 -0400

    initialized the file and stored all the data in memory

---
+++ stack2.asm
@@ -0,0 +1,5 @@
+.data			#used to load data in the memory
+nm: .word 24,72,6,18,43,37	#6 random integers will be stored in the consequitive memory
+mm: .word 5:8			#stores 8 5's in the memory right after the 6 random numbers
+jj: .space 20			#provides empty space of 20 bytes right after the 8 5's
+gg: .word 122			#stores 122 in the memory after the empty space
