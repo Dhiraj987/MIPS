43aa50db47f2f7462c33246eb7ba6d06c20d5161
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-04-22 11:46:52 -0400

    created random integer generator

--- EvenOddModifier.asm
+++ EvenOddModifier.asm
@@ -4,8 +4,25 @@
 la $s0,aa	#loading the first empty space that we need for random integers
 la $s3,bb	#loading the second empty space we need to store the modified results
 
+LOOP:
+li	$a0, 1		#random generator id
+li	$a1, 710	# upper bound of the range: (max-min)
+li	$v0, 42		# random int range
+syscall
+
+addi	$s6, $s6, 1	# increment the number of iterations
+beq	$s6, $s7, step1	# branch to step1 if iterations is 12
 
 
+addi $t4,$a0,58		#setting the lower bound of the randomly generated integers
+
+sw $t4,0($s0)		#storing the random numbers as an array
+addi $s0,$s0,4		#increasing the placements of the array
+
+
+j	LOOP	# Do another iteration 
+
+step1:
 
 .data
 aa:	.space 48
