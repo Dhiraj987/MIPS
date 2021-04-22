39959cebcc9590bd119f159500b7b25987e9c5ac
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-04-22 11:40:52 -0400

    added space for the two empty arrays

---
+++ EvenOddModifier.asm
@@ -0,0 +1,12 @@
+.text
+
+
+la $s0,aa	#loading the first empty space that we need for random integers
+la $s3,bb	#loading the second empty space we need to store the modified results
+
+
+
+
+.data
+aa:	.space 48
+bb:	.space 48
